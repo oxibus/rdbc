79315d5c01c8b0dda0f075e8bcbf551b6548d30a
huangjian <huangjian@bicv.com>
2024-10-15 14:21:19 +0800

    feat: add parser dbc file

--- src/bin/dbc2json.rs
+++ src/bin/dbc2json.rs
@@ -1,12 +1,15 @@
 use anyhow::Result;
-use rrdbc::ast::network_ast::*;
-use rrdbc::file::read_file_content;
+use rrdbc::file::parser_dbc_file;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "dbc2json", about = "Convert DBC file to JSON")]
 struct Opt {
+    /// Input file encoding
+    #[structopt(short, long, default_value = "UTF-8")]
+    encoding: String,
+
     /// Input dbc file
     #[structopt(short, long, parse(from_os_str))]
     input: PathBuf,
@@ -19,8 +22,7 @@ struct Opt {
 fn main() -> Result<()> {
     env_logger::init();
     let opt = Opt::from_args();
-    let data = read_file_content(opt.input.to_str().unwrap())?;
-    let network_ast = parse_dbc(&data)?;
+    let network_ast = parser_dbc_file(&opt.input.to_str().unwrap(), &opt.encoding)?;
     let network_ast_json = serde_json::to_string_pretty(&network_ast)?;
     std::fs::write(opt.output, network_ast_json)?;
     Ok(())
--- src/file.rs
+++ src/file.rs
@@ -1,59 +1,25 @@
+use crate::ast::network_ast::parse_dbc;
+use crate::ast::network_ast::NetworkAst;
+use crate::encoding::to_utf8;
 use anyhow::Result;
-use encoding_rs::GBK;
-use encoding_rs::WINDOWS_1252;
 use std::fs::File;
 use std::io::Read;
 
-pub fn try_read_file_content_with_utf8(filename: &str) -> Result<String> {
-    let data = std::fs::read_to_string(filename)?;
+pub fn read_file_content(filename: &str, encoding: &str) -> Result<String> {
+    let data = if encoding.to_lowercase() == "utf-8" {
+        std::fs::read_to_string(filename)?
+    } else {
+        let mut file = File::open(filename)?;
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer)?;
+        to_utf8(encoding, &buffer)?
+    };
+
     Ok(data)
 }
 
-pub fn try_read_file_content_with_gbk(filename: &str) -> Result<String> {
-    let mut file = File::open(filename)?;
-
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer)?;
-
-    let (cow, _, had_errors) = GBK.decode(&buffer);
-    if had_errors {
-        return Err(anyhow::anyhow!("GBK decode had errors"));
-    }
-
-    Ok(cow.to_string())
-}
-
-pub fn try_read_file_content_with_window_1252(filename: &str) -> Result<String> {
-    let mut file = File::open(filename)?;
-
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer)?;
-
-    let (cow, _, had_errors) = WINDOWS_1252.decode(&buffer);
-    if had_errors {
-        return Err(anyhow::anyhow!("WINDOWS_1252 decode had errors"));
-    }
-
-    Ok(cow.to_string())
-}
-
-pub fn read_file_content(filename: &str) -> Result<String> {
-    let data = try_read_file_content_with_utf8(filename);
-    if data.is_ok() {
-        return data;
-    }
-
-    let data = try_read_file_content_with_gbk(filename);
-    if data.is_ok() {
-        return data;
-    }
-
-    let data = try_read_file_content_with_window_1252(filename);
-    if data.is_ok() {
-        return data;
-    }
-
-    Err(anyhow::anyhow!(
-        "read file content failed, file encoding is not supported"
-    ))
+pub fn parser_dbc_file(filename: &str, encoding: &str) -> Result<NetworkAst> {
+    let data = read_file_content(filename, encoding)?;
+    let network_ast = parse_dbc(&data)?;
+    Ok(network_ast)
 }
